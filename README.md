20ea3f9ff763eeceb180b068caf3b5056b513e35
felpzxxxxxx <20261ctb0100053@estudantes.ifpr.edu.br>
2026-04-07 17:17:54 +0000

    Aula 10 - teste.py: explorando print e input

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # 2026-PCAP
 Repositório de Felipe Gravina
+'''
+
+Problema: beecrowd 
+Data:
+Estudante:
+
+'''
+
+# Objetivo: descrição sucinta do que o programa deve fazer
+
+
+# --- ANÁLISE (LIAC) ---
+# Enttrada: 
+# Processamento:
+# Saída: 
---
+++ beecrowd/teste.py
@@ -0,0 +1,17 @@
+
+#  Experimento 1 - Saída de dados
+print("Olá, mundo!")
+print("Meu nome é Python 🐍")
+
+
+# Experimento 2 - Entrada de dados
+nome = input("Qual é o seu nome? ")
+print("Olá, "+ nome + "!")
+
+
+# Experimento 3 - Operação simples 
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
